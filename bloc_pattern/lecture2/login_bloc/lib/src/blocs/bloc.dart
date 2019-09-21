845af1fe0edbb6b3ec06515d36528e56dea22b6b
Murad Alwan <free1soft@gmail.com>
2019-09-21 03:17:59 +0300

    Login Example -not Complete

---
+++ bloc_pattern/lecture2/login_bloc/lib/src/blocs/bloc.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'validator.dart';
+
+class Bloc extends Object with Validator{
+  final _emailController=StreamController<String>();
+  final _passwordController=StreamController<String>();
+
+  
+  //set data
+  Function(String) get changeEmail=>_emailController.sink.add;
+  Function(String) get changePassword=>_passwordController.sink.add;
+
+  //get data
+  Stream<String> get email=>_emailController.stream.transform(validateEmail);
+  Stream<String> get password=>_passwordController.stream.transform(validatePassword);
+
+  dispose(){
+    _emailController.close();
+    _passwordController.close();
+  }
+
+  
+}
+
+final bloc=Bloc();
